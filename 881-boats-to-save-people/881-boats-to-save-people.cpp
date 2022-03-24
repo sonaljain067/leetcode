67082eb33e9c7a9cd3128e08b7791ed2ef19b633
Sonal Jain <52230497+sonaljain067@users.noreply.github.com>
2022-03-24 20:37:07 +0530

    Time: 176 ms (7.75%), Space: 41.9 MB (57.42%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
         sort(people.begin(),people.end());
-        int i=0, j=people.size()-1, cnt=0;
+        int i=0,j=people.size()-1,cnt=0;
         while(i<=j){
+            //lighest and heaviest person sum <= limit
             if(people[i]+people[j]<=limit){
-                ++i; --j; 
+                ++i; --j;
             }
-            else{
-                --j;
-            }
+            //if sum is beyond limit, heaviest person will go alone
+            else --j;
             cnt++;
         }
         return cnt;
