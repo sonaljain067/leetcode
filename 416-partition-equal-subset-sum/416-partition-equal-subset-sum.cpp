e9fd45ebe1bbe3b73ebdf668dbb28f0ce68a72cb
Sonal Jain <52230497+sonaljain067@users.noreply.github.com>
2022-03-25 20:42:28 +0530

    Time: 88 ms (87.85%), Space: 11.4 MB (67.44%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+        //Using Recursion
+//     bool PartitionRecursion(vector<int> &nums, int n, int s){
+//         if(n==0) return 0;
+//         if(s==0) return 1;
+
+//         if(nums[n-1]<=s)
+//             return PartitionRecursion(nums,n-1,s-nums[n-1]) || PartitionRecursion(nums,n-1,s);
+//         else return PartitionRecursion(nums,n-1,s);
+//     }
+    
+        // Using Memoization
+    // bool PartitionMemoization(vector<int> &nums, int n, int s){
+    //     if(n==0) return 0;
+    //     if(s==0) return 1;
+    //     bool dp[n+1][s+1]; 
+    //     if(dp[n][s]!=-1) return dp[n][s];
+    //     if(nums[n-1]<=s)
+    //         return dp[n][s]=PartitionMemoization(nums,n-1,s-nums[n-1]) || PartitionMemoization(nums,n-1,s);
+    //     else return PartitionMemoization(nums,n-1,s);
+    // }
+    
     bool canPartition(vector<int>& nums) {
         //Using tabulation
         int sum=0, n=nums.size(); 
@@ -7,7 +28,9 @@ public:
         
         if(sum%2!=0) 
             return false;
-        
+        // return PartitionRecursion(nums,n,sum/2);
+        // return PartitionMemoization(nums,n,sum/2);
+        // Using Tabulation
         bool t[n+1][sum/2+1];
         for(int i=0;i<n+1;i++) t[i][0]=true;
         for(int j=0;j<sum/2+1;j++) t[0][j]=false;
