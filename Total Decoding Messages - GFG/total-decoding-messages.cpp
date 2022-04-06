a68c019ba2f506c6d0f205b77a82ce5f70b28202
Sonal Jain <52230497+sonaljain067@users.noreply.github.com>
2022-04-06 13:01:12 +0530

    Added solution - LeetHub

---
+++ Total Decoding Messages - GFG/total-decoding-messages.cpp
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+public:
+	int CountWays(string str){
+	   int n=str.size(); const int mod=1e9+7;
+	   vector<int>dp(n+1,0); dp[0]=dp[1]=1;
+	   if(str[0]=='0') return 0;
+	   
+	   
+	   for(int i=2;i<=n;i++) {
+	       dp[i]=0;
+	       if(str[i-1]>'0') dp[i]=dp[i-1]%mod;
+	       if(str[i-2]=='1' || str[i-2]=='2' && str[i-1]<'7') 
+	            dp[i]=(dp[i]+dp[i-2])%mod;
+	   }
+	   return dp[n];
+	}
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string str;
+		cin >> str;
+		Solution obj;
+		int ans = obj.CountWays(str);
+		cout << ans << "\n";
+	}
+	return 0;
+}  // } Driver Code Ends
