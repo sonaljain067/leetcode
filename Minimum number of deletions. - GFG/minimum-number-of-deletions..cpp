c89547ff5948a3fc3ee7c05862f8ed6a51f2f039
Sonal Jain <52230497+sonaljain067@users.noreply.github.com>
2022-04-12 15:58:39 +0530

    Added solution - LeetHub

--- Minimum number of deletions. - GFG/minimum-number-of-deletions..cpp
+++ Minimum number of deletions. - GFG/minimum-number-of-deletions..cpp
@@ -19,7 +19,7 @@ return 0;
 }// } Driver Code Ends
 
 int LCS(string &s1, string &s2){
-    int n=s1.size(), m=n;
+    int n=s1.size(), m=s2.size();
     vector<vector<int>> dp(n+1, vector<int>(m+1,0));
     for(int i=0;i<n+1;i++){
         for(int j=0;j<m+1;j++){
