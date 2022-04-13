338033d14fa6d55d1ad9cbb4a5f70372b8e759fb
Sonal Jain <52230497+sonaljain067@users.noreply.github.com>
2022-04-13 14:41:46 +0530

    Added solution - LeetHub

--- Palindromic patitioning - GFG/palindromic-patitioning.cpp
+++ Palindromic patitioning - GFG/palindromic-patitioning.cpp
@@ -9,7 +9,6 @@ using namespace std;
 
 class Solution{
 public:
-    int dp[501][501];
     bool isPalindrome(string &str,int i,int j){
         while(i<j){
             if(str[i]!=str[j]) return false;
@@ -17,19 +16,23 @@ public:
         }
         return true;
     }
-    int MCM(string &str,int i,int j){
+    int MCM(string &str,int i,int j,vector<vector<int>>&dp){
         if(i>=j) return dp[i][j]=0;
         if(isPalindrome(str,i,j)==true) return 0;
         if(dp[i][j]!=-1) return dp[i][j]; 
         int ans=INT_MAX;
         
-        for(int k=i;k<=j-1;k++)
-            ans=min(ans,MCM(str,i,k)+1+MCM(str,k+1,j));
+        for(int k=i;k<=j-1;k++){
+            int temp=MCM(str,i,k,dp)+1+MCM(str,k+1,j,dp);
+            if(temp<ans)
+                ans=temp;
+        }
         return dp[i][j]=ans;
     }
     int palindromicPartition(string str){
-        int n=str.length(); memset(dp,-1,sizeof(dp));
-        return MCM(str,0,n-1);
+        int n=str.length(); 
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return MCM(str,0,n-1,dp);
     }
 };
 
