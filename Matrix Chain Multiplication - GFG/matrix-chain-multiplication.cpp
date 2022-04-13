81e013406dd8a5f0dad2a3b03d51a517441405fc
Sonal Jain <52230497+sonaljain067@users.noreply.github.com>
2022-04-13 13:35:36 +0530

    Added solution - LeetHub

---
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    // TC: O(n^3), S:O(n^2)
+    int MCM(int arr[], int i, int j,vector<vector<int>>&dp){
+        if(i>=j) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        int ans=INT_MAX;
+        
+        for(int k=i;k<=j-1;k++){
+            int temp=MCM(arr,i,k,dp)+MCM(arr,k+1,j,dp)+(arr[i-1]*arr[k]*arr[j]);
+            if(temp<ans)
+                ans=temp;
+        }
+        return dp[i][j]=ans;
+        return ans;
+    }
+    int matrixMultiplication(int N, int arr[]){
+        vector<vector<int>> dp(N+1,vector<int>(N+1,-1));
+        return MCM(arr,1,N-1,dp);
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        cout<<ob.matrixMultiplication(N, arr)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
