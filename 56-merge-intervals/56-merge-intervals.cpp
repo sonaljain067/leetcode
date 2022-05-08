494ea8bc89e178b3cd4789010fddd269936d9a56
Sonal Jain <52230497+sonaljain067@users.noreply.github.com>
2022-05-08 23:43:05 +0530

    Time: 48 ms (56.56%), Space: 18.9 MB (85.42%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -4,20 +4,12 @@ public:
         vector<vector<int>> vec;
         if(intervals.size()<=1) return intervals;
         sort(intervals.begin(),intervals.end());
-        // vector<int> res=intervals[0];
-        // for(auto it: intervals){
-        //     if(it[0]<=res[1]) res[1]=max(it[1],res[1]);
-        //     else{
-        //         vec.push_back(res); res=it;
-        //     }
-        // }
         vec.push_back(intervals[0]);
         for(int i=1;i<intervals.size();i++){
             if(vec.back()[1]>=intervals[i][0])
                 vec.back()[1]=max(vec.back()[1],intervals[i][1]);
             else vec.push_back(intervals[i]);
         }
-        // vec.push_back(res);
         return vec;
     }
 };
