7fbf514760b0808e4065ebfae59a4867daac56db
Sonal Jain <52230497+sonaljain067@users.noreply.github.com>
2022-05-10 21:44:32 +0530

    Time: 4 ms (34.38%), Space: 6.8 MB (20.91%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void combinationSum(vector<int> temp,int target,int sum,int id,vector<vector<int>> &ans){ 
-        // if(sum==target ){
-        //     ans.push_back(temp); return;
-        // }
-        // if(temp.size()<k){
-        //     for(int i=temp.empty() ? 1 : temp.back()+1; i<=9;i++){
-        //         if()
-        //     }
-        // }
-           
-    }
     int sum(vector<int>&combine){
         int tar=0; 
         for(auto ele: combine)
@@ -19,15 +8,14 @@ public:
         return tar;
     }
     void combination(int k,int n,vector<int>& combine,int num){
-        
         if(k==0){
             if(sum(combine)==n) ans.push_back(combine);
             return;
         }
         if(num>9) return;
-        combine.push_back(num);
-        combination(k-1,n,combine,num+1);
-        combine.pop_back();
+        combine.push_back(num); //Including num in combine
+        combination(k-1,n,combine,num+1); // 
+        combine.pop_back(); // Not including num in combine
         combination(k,n,combine,num+1);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
