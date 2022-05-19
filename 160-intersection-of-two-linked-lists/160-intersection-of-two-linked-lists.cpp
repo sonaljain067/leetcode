fa384bd852a2eb91fc785fc107cb7b1ac95f4477
Sonal Jain <52230497+sonaljain067@users.noreply.github.com>
2022-05-20 00:38:16 +0530

    Time: 57 ms (58.38%), Space: 14.4 MB (99.02%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -49,31 +49,31 @@ public:
         // return NULL;
         
         // Way: Hashmap TC: O(n+m), SC: O(m)
-        unordered_map<ListNode*,int> mp; 
-        while(headA!=NULL){
-            mp[headA]++; headA=headA->next;
-        }
-        while(headB!=NULL){
-            if(mp[headB]>0) return headB;
-            headB=headB->next;
-        }
-        return NULL;
+        // unordered_map<ListNode*,int> mp; 
+        // while(headA!=NULL){
+        //     mp[headA]++; headA=headA->next;
+        // }
+        // while(headB!=NULL){
+        //     if(mp[headB]>0) return headB;
+        //     headB=headB->next;
+        // }
+        // return NULL;
         
         // Way: Length Difference TC: O(m+n),SC: O(1)
-//         int n=0,m=0; ListNode *ptr1=headA, *ptr2=headB;
-//         while(ptr1!=NULL){ n++; ptr1=ptr1->next; }
-//         while(ptr2!=NULL){ m++; ptr2=ptr2->next; }
-//         int t=abs(n-m);
-//         if(n>m){
-//             while(t){headA=headA->next; t--;}
-//         }
-//         else { while(t){ headB=headB->next; t--; }}
+        int n=0,m=0; ListNode *ptr1=headA, *ptr2=headB;
+        while(ptr1!=NULL){ n++; ptr1=ptr1->next; }
+        while(ptr2!=NULL){ m++; ptr2=ptr2->next; }
+        int t=abs(n-m);
+        if(n>m){
+            while(t){headA=headA->next; t--;}
+        }
+        else { while(t){ headB=headB->next; t--; }}
         
-//         while(headA!=NULL && headB!=NULL){
-//             if(headA==headB) return headB;
-//             headA=headA->next; headB=headB->next;
-//         }
-//         return NULL;
+        while(headA!=NULL && headB!=NULL){
+            if(headA==headB) return headB;
+            headA=headA->next; headB=headB->next;
+        }
+        return NULL;
         
         // Way: Two Pointers TC: O(n+m), SC O(1)
         // ListNode *ptr1=headA, *ptr2=headB;
