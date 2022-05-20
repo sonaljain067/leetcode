1d8918fc8397d72d917a0be9f1e4adf32ef47c2a
Sonal Jain <52230497+sonaljain067@users.noreply.github.com>
2022-05-20 21:07:55 +0530

    Time: 35 ms (8.62%), Space: 11.4 MB (73.00%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -12,14 +12,14 @@ class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
         // Way: Recursion TC: O(), SC O(n)
-        // ListNode *cursor=head, *prev=NULL, *curr=head, *nxt=NULL;
-        // for(int i=0;i<k;i++) { if(!cursor) return head; cursor=cursor->next; }
-        // for(int i=0;i<k;i++){
-        //     nxt=curr->next; curr->next=prev;
-        //     prev=curr; curr=nxt;
-        // }
-        // head->next=reverseKGroup(curr,k);
-        // return prev;
+        ListNode *cursor=head, *prev=NULL, *curr=head, *nxt=NULL;
+        for(int i=0;i<k;i++) { if(!cursor) return head; cursor=cursor->next; }
+        for(int i=0;i<k;i++){
+            nxt=curr->next; curr->next=prev;
+            prev=curr; curr=nxt;
+        }
+        head->next=reverseKGroup(curr,k);
+        return prev;
         
         
         // Way: Recursion TC O(), SC O(n)
@@ -34,20 +34,20 @@ public:
         
         
         // Way: Two pointers/Iterative TC: O(), SC: O(1)
-        ListNode *dummy=new ListNode(0),*bef=dummy, *aft=head, *curr=NULL, *prev=NULL, *forw=NULL; 
-        dummy->next=head; 
-        while(true){
-            ListNode *cursor=aft; 
-            for(int i=0;i<k;i++){
-                if(!cursor) return dummy->next; cursor=cursor->next;
-            }
-            curr=aft; prev=bef;
-            for(int i=0;i<k;i++){
-                forw=curr->next; curr->next=prev;
-                prev=curr; curr=forw;
-            }
-            aft->next=curr; bef->next=prev;bef=aft; aft=curr;
-        }
+        // ListNode *dummy=new ListNode(0),*bef=dummy, *aft=head, *curr=NULL, *prev=NULL, *forw=NULL; 
+        // dummy->next=head; 
+        // while(true){
+        //     ListNode *cursor=aft; 
+        //     for(int i=0;i<k;i++){
+        //         if(!cursor) return dummy->next; cursor=cursor->next;
+        //     }
+        //     curr=aft; prev=bef;
+        //     for(int i=0;i<k;i++){
+        //         forw=curr->next; curr->next=prev;
+        //         prev=curr; curr=forw;
+        //     }
+        //     aft->next=curr; bef->next=prev;bef=aft; aft=curr;
+        // }
     }
 };
 
