3fd298254cd0796b6d45656d082a1587ae2a1761
Sonal Jain <52230497+sonaljain067@users.noreply.github.com>
2022-05-24 22:34:54 +0530

    Time: 24 ms (23.92%), Space: 18.3 MB (75.01%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // Way1.1: Two pointer TC: O(n), SC O(1)
+        //Way1: Hashset TC: O(n), SC: O(n)
+        // int n=nums.size(),i=0; set<int> st;
+        // for(int i=0;i<n;i++) st.insert(nums[i]);
+        // int ans=st.size();
+        // for(auto x: st) nums[i++]=x;
+        // return ans;
+        
+        // Way2.1: Two pointer TC: O(n), SC O(1)
         // int ans=1, n=nums.size();
         // if(n==0) return 0;
         // for(int i=1;i<n;i++)
         //     if(nums[i-1]!=nums[i]) nums[ans++]=nums[i];    
         // return ans;
         // or 
-        // Way1.2: Two pointer TC: O(n), SC O(1)
-        // int n=nums.size(),left=1;; if(n==0) return 0;
-        // for(int right=1; right<n;right++){
-        //     if(nums[left-1]!=nums[right]) nums[left++]=nums[right];
-        // }
-        // return left;
-        
-        //Way: Hashset TC: O(n), SC: O(n)
-        int n=nums.size(),i=0; set<int> st;
-        for(int i=0;i<n;i++) st.insert(nums[i]);
-        int ans=st.size();
-        for(auto x: st) nums[i++]=x;
-        return ans;
+        // Way2.2: Two pointer TC: O(n), SC O(1)
+        int n=nums.size(),left=1;; if(n==0) return 0;
+        for(int right=1; right<n;right++){
+            if(nums[left-1]!=nums[right]) nums[left++]=nums[right];
+        }
+        return left;
     }
 };
