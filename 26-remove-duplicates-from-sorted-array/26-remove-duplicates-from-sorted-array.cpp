2dd3a780b934b7dce2172e4042af1d986c6e8a6c
Sonal Jain <52230497+sonaljain067@users.noreply.github.com>
2022-05-24 22:43:44 +0530

    Time: 11 ms (84.67%), Space: 18.4 MB (75.01%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        //Way1: Hashset TC: O(n), SC: O(n)
+        //Way1: Hashset TC: O(nlogn), SC: O(n)
         // int n=nums.size(),i=0; set<int> st;
         // for(int i=0;i<n;i++) st.insert(nums[i]);
         // int ans=st.size();
         // for(auto x: st) nums[i++]=x;
         // return ans;
         
-        // Way2.1: Two pointer TC: O(n), SC O(1)
-        // int ans=1, n=nums.size();
-        // if(n==0) return 0;
-        // for(int i=1;i<n;i++)
-        //     if(nums[i-1]!=nums[i]) nums[ans++]=nums[i];    
-        // return ans;
-        // or 
-        // Way2.2: Two pointer TC: O(n), SC O(1)
-        int n=nums.size(),left=1;; if(n==0) return 0;
-        for(int right=1; right<n;right++){
-            if(nums[left-1]!=nums[right]) nums[left++]=nums[right];
+        //Way2: Hashmap TC: O(nlogn), SC: O(n)
+        // vector<int> ans; map<int,int> mp; int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     if(mp[nums[i]]==0) ans.push_back(nums[i]);
+        //     mp[nums[i]]++;
+        // }
+        // nums=ans;
+        // return ans.size();
+        
+        // Way3.1: Two pointer TC: O(n), SC O(1)
+        // int n=nums.size(),left=1;; if(n==0) return 0;
+        // for(int right=1; right<n;right++){
+        //     if(nums[left-1]!=nums[right]) nums[left++]=nums[right];
+        // }
+        // return left;
+        
+        // Way3.2: Two pointer TC: O(n), SC O(1)
+        int n=nums.size(),i=1,j=1; if(n<=1) return n;
+        while(j<n){
+            if(nums[j-1]==nums[j]) j++;
+            else nums[i++]=nums[j++];
         }
-        return left;
+        return i;
+        
+        
+        //Way4: STL function
+        // nums.erase(unique(nums.begin(),nums.end()),nums.end());
+        // return nums.size();
+        
     }
 };
