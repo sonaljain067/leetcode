afcf7084ce3fa7d6ed6a04d47ae67b0a8d2d772d
Sonal Jain <52230497+sonaljain067@users.noreply.github.com>
2022-06-12 01:02:12 +0530

    Time: 293 ms (55.52%), Space: 98.5 MB (71.00%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,24 +2,34 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         // Way1: Hashmap TC: O(n), SC: O(n)
-        unordered_map<int,int> umap; int n=nums.size(),ans=INT_MAX;
+//         unordered_map<int,int> umap; int n=nums.size(),ans=INT_MAX;
         
-        for(int i=0,prefix=0;i<n;i++){
-            prefix+=nums[i];
-            umap[prefix]=i+1;
-            if(prefix==x) ans=min(ans,i+1);
+//         for(int i=0,prefix=0;i<n;i++){
+//             prefix+=nums[i];
+//             umap[prefix]=i+1;
+//             if(prefix==x) ans=min(ans,i+1);
+//         }
+        
+//         for(int i=n-1,suffix=0;i>=0;i--){
+//             suffix+=nums[i];
+//             if(suffix==x) ans=min(ans,n-i);
+//             if(umap[x-suffix]>0 && i+1>umap[x-suffix])
+//                 ans=min(ans,n-i+umap[x-suffix]);
+//         }
+        
+//         return ans==INT_MAX ? -1 : ans;
+        
+        // Way2: Longest subarray CT: O(n), SC: O(1)
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        if(sum<x) return -1;
+        if(sum==x) return nums.size();
+        int target=sum-x,curSum=0,start=0,maxSize=0,n=nums.size();
+        for(int i=0;i<n;i++){
+            curSum+=nums[i];
+            while(curSum>target) curSum-=nums[start++];
+            if(curSum==target) maxSize=max(maxSize,i-start+1);
         }
-        
-        for(int i=n-1,suffix=0;i>=0;i--){
-            suffix+=nums[i];
-            if(suffix==x) ans=min(ans,n-i);
-            if(umap[x-suffix]>0 && i+1>umap[x-suffix])
-                ans=min(ans,n-i+umap[x-suffix]);
-        }
-        
-        return ans==INT_MAX ? -1 : ans;
-        
-        // Way2: Longest subarray 
+        return (maxSize==0) ? -1 : n-maxSize;
     }
 };
 
